ad7acee48eecd6b00bb5cd48482288d7b5ca195f
Sharafudheen <sahara.sharafu@gmail.com>
2020-11-02 23:54:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './index.css'
+
+function App() {
+  return (
+   <div className="grid-container">
+     <header>
+       <a href="/">Shoping Cart </a> 
+     </header>
+
+     <main>
+       Product List
+     </main>
+
+     <footer>
+       Alright is preserverd!
+     </footer>
+
+   </div>
+
+  );
+}
+
+export default App;
