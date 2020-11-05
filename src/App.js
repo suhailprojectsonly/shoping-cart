2a29029d9cc1a2888b85807234d11402cfeb95b0
suhailprojectsonly <suhailprojectsonly@gmail.com>
2020-11-05 21:51:13 +0530

    cart-added

--- src/App.js
+++ src/App.js
@@ -1,61 +1,94 @@
 //feature
 
 import React from "react";
+import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
 
 class App extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);  
     this.state = {
       products: data.products,
+      cartItems: [],
       sort: "",
-    }; 
+    };
   }
 
-  sortProducts= (event)=>{
-    
+  removeFromCart = (product) => {
+
+    const cartItems = this.state.cartItems.slice();
+    this.setState({cartItems:cartItems.filter(x=>x._id !== product._id)});
+  }
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({
+        ...product,
+        count: 1,
+      });
+    }
+    this.setState({
+      cartItems,
+    });
+  };
+
+  sortProducts = (event) => {
     const sort = event.target.value;
     console.log(event.target.value);
 
     this.setState((state) => ({
-      
-      sort: sort, 
-      products : this.state.products.slice().sort((a,b)=>
-        sort === "lowest"? 
-        a.price > b.price ? 1:-1:
-        sort === "highest"? 
-        a.price < b.price ? 1:-1:
-        a.id < b.id ? 1:-1
-      ), 
-
-        
-    }));  
-
-  };    
-
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
 
   render() {
     return (
       <div className="grid-container">
         <header>
           <a href="/">Shoping Cart </a>
-        </header> 
+        </header>
 
         <main>
           <div className="content">
             <div className="main">
-<Filter count={this.state.products.length}
- sort ={this.state.sort }
-sortProducts={this.sortProducts}
-></Filter>
-              <Products products={this.state.products}></Products>
-
+              <Filter
+                count={this.state.products.length}
+                sort={this.state.sort}
+                sortProducts={this.sortProducts}
+              ></Filter>
+              <Products products={this.state.products} addToCart={this.addToCart} >            
+              </Products>
             </div>
 
-            <div className="sidebar">Cart Items</div>
-          </div> 
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems} removeFromCart={this.removeFromCart}/>
+            </div>
+          </div>
         </main>
 
         <footer>Alright is preserverd!</footer>
