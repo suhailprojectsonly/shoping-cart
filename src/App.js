3b3485e22dec50cd80834fa223b7a88c3f329359
suhailprojectsonly <suhailprojectsonly@gmail.com>
2020-11-05 15:11:07 +0530

    filter_added

--- src/App.js
+++ src/App.js
@@ -14,9 +14,27 @@ class App extends React.Component {
     }; 
   }
 
-  sortProduct(event){
+  sortProducts= (event)=>{
+    
+    const sort = event.target.value;
+    console.log(event.target.value);
 
-  }
+    this.setState((state) => ({
+      
+      sort: sort, 
+      products : this.state.products.slice().sort((a,b)=>
+        sort === "lowest"? 
+        a.price > b.price ? 1:-1:
+        sort === "highest"? 
+        a.price < b.price ? 1:-1:
+        a.id < b.id ? 1:-1
+      ), 
+
+        
+    }));  
+
+  };    
+
 
   render() {
     return (
@@ -28,19 +46,16 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-<Filter count={this.state.products.length}>
-
-sort ={this.state.sort }
-filterProducts={this.filterProducts}
-
-
-</Filter>
+<Filter count={this.state.products.length}
+ sort ={this.state.sort }
+sortProducts={this.sortProducts}
+></Filter>
               <Products products={this.state.products}></Products>
 
             </div>
 
             <div className="sidebar">Cart Items</div>
-          </div>
+          </div> 
         </main>
 
         <footer>Alright is preserverd!</footer>
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -7,7 +7,7 @@ export default class Filter extends Component {
         <div className="filter-result">{this.props.count}Products</div>
         <div className="filter-sort">
           Order{" "}
-          <select>
+          <select value={this.props.sort} onChange={this.props.sortProducts} >
             <option>Latest</option>
             <option value="lowest">Low Price</option>
             <option value="highest">High Price</option>
